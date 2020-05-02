0fac9f50c5bf3d8218327447f51ccc30a4f89ab5
garug <allefff@gmail.com>
2020-05-02 20:41:39 -0300

    Post for create a transaction works

---
+++ src/database/migrations/1588438482683-InitialDatabase.ts
@@ -0,0 +1,90 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+
+export default class InitialDatabase1588438482683
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'numeric',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey('transactions', new TableForeignKey({
+      name: 'TransactionCategory',
+      columnNames: ['category_id'],
+      referencedTableName: 'categories',
+      referencedColumnNames: ['id'],
+      onDelete: 'SET NULL',
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+    await queryRunner.dropTable('categories');
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,51 @@
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+import { getRepository } from 'typeorm';
+
+interface Entry {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Entry): Promise<Transaction> {
+    const repository = getRepository(Transaction);
+    const category_id = (await this.getCategory(category)).id;
+
+    const transaction = repository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+    return repository.save(transaction);
+  }
+
+  private async getCategory(category: string): Promise<Category> {
+    const repository = getRepository(Category);
+
+    const savedCategory = await repository
+      .createQueryBuilder('category')
+      .where('category.title = :title', { title: category })
+      .getOne();
+
+    if (savedCategory) {
+      return savedCategory;
+    } else {
+      const categoryToSave = repository.create({
+        title: category,
+      });
+      return repository.save(categoryToSave);
+    }
   }
 }
 
